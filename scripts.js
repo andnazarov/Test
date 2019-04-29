fa0f4b094dcfdc2149721d5851e54c16da86c063
andnazarov <nazaroff.and@gmail.com>
2019-04-29 23:04:12 +0300

    1

--- scripts.js
+++ scripts.js
@@ -1,17 +1,26 @@
 jQuery(document).ready(function ($) {
 
-    $('[name="listItem"]').click(function(event){
+    $('[name="listItem"]').click(function (event) {
         //alert(event.delegateTarget.id);
         $('#exampleModal').modal('show');
     });
-    
+
     //$('#content').clear();
-    
-    //$('#content').load('navbar.ejs');
-    $('#content').on('load', function() {
-        this.append('<p>тест</p>');
+
+    $('#navbar').ready(function (event) {
+        $('#navbar').empty();
+        //var template = $this.get("navbar.ejs");
+        $('#navbar').load('navbar.ejs');
     });
+
     
+
+    //$('#content').ready(function (event) {
+    //    $('#content').empty();
+    //    var template = $.get("navbar.ejs");
+    //    $('#content').html()
+    //});
+
 
 });
 
