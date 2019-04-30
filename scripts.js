75db9f6e139a51ea816005fcae593e4e9563e26b
andnazarov <nazaroff.and@gmail.com>
2019-04-30 22:30:55 +0300

    1

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
 jQuery(document).ready(function ($) {
 
-    
 
-    
-    $.get("navbar.html", function(data) {
-        var ht = ejs.render(data, {navItem: "Привет!"});
-        $('#navbar').html(ht);
+
+
+    $.get("navbar.html", function (template) {
+        $.get("http://localhost/DemoSSL/hs/jqmob/navbar"), function (datajson, template) {
+            var data = JSON.parse(datajson);
+            var ht = ejs.render(template, { navItemList: data });
+            $('#navbar').html(ht);
+        }
     });
-       
+
     var itemList = [
-        {id: 'item1', caption: 'Тест 1', description: 'Дополнительное описание тест 1', isActive: 'active'},
-        {id: 'item2', caption: 'Тест 2', description: 'Дополнительное описание тест 2', isActive: ''}
-        ];
-    $.get("listform.html", function(data) {
-        var ht = ejs.render(data, {itemList: itemList});
+        { id: 'item1', caption: 'Тест 1', description: 'Дополнительное описание тест 1', isActive: 'active' },
+        { id: 'item2', caption: 'Тест 2', description: 'Дополнительное описание тест 2', isActive: '' }
+    ];
+    $.get("listform.html", function (data) {
+        var ht = ejs.render(data, { itemList: itemList });
         $('#listform').html(ht);
         $('[name="listItem"]').click(function (event) {
             $('#exampleModal').modal('show');
