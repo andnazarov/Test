1475211dbae9d58d08741d5bde1373da5c448a31
andnazarov <nazaroff.and@gmail.com>
2019-06-10 22:36:25 +0300

    1

--- scripts_ques.js
+++ scripts_ques.js
@@ -6,11 +6,13 @@ jQuery(document).ready(function ($) {
         
     
         $.get("questions_list.html", function (data) {
-            var ht = ejs.render(data, { itemList: itemList, divName: "Раздел 01" });
+            var divName = $_GET("div");
+            var ht = ejs.render(data, { itemList: itemList, divName: divName });
+            
             $('#listform').html(ht);
             $('[name="listItem"]').click(function (event) {
                 
-                alert(this);
+                //alert(this);
             });
         });
     
@@ -21,3 +23,17 @@ jQuery(document).ready(function ($) {
 
 });
 
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace( location.hash, '' ).replace( 
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;	
+	}
+	return vars;
+}
