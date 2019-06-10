6417204c1e8f79f6aa039e03f75024364ecd09fe
root <nazaroff.and@gmail.com>
2019-06-10 22:08:40 +0300

    1

---
+++ scripts_ques.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function ($) {
+    
+    $.get("questions01.json", function (datajson) {
+        console.log(datajson);
+        var itemList = datajson;//JSON.parse(datajson);
+        
+    
+        $.get("questions_list.html", function (data) {
+            var ht = ejs.render(data, { itemList: itemList; divName: "Раздел 01" });
+            $('#listform').html(ht);
+            $('[name="listItem"]').click(function (event) {
+                //$('#exampleModal').modal('show');
+                alert(this);
+            });
+        });
+    
+    });
+
+    
+    
+
+});
+
