4e26bb1c32efc3f35080197bef5b540fc5e85308
root <nazaroff.and@gmail.com>
2019-06-11 18:30:51 +0300

    1

--- test.js
+++ test.js
@@ -15,3 +15,23 @@ jQuery(document).ready(function ($) {
     });
     
 });
+
+$( "*" ).on( "pagecreate", function( event ) {
+
+    $.get("divisions.json", function (datajson) {
+        var itemList = datajson;//JSON.parse(datajson);
+        
+        var divsList = $("#divsList");
+        
+        itemList.forEach(function(item, i, itemList) {
+           
+           var element = '<li id="div' + item.id + '"><a href="#">' + item.id + '. ' + item.caption + '<span class="ui-li-count">12</span></a></li>';
+           divsList.append(element);
+            
+        });
+        
+        divsList.listview("refresh");
+    
+    });
+
+});
