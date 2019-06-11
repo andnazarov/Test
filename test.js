c9d1a114efd53a3bceee23c10d87d2c088b56690
root <nazaroff.and@gmail.com>
2019-06-11 18:19:24 +0300

    1

--- test.js
+++ test.js
@@ -3,7 +3,14 @@ jQuery(document).ready(function ($) {
     $.get("divisions.json", function (datajson) {
         var itemList = datajson;//JSON.parse(datajson);
         
+        var divsList = $("divsList");
         
+        itemList.forEach(function(item, i, itemList) {
+           
+           var element = '<li id="div' + item.id + '"><a href="#">' + item.id + '. ' + item.caption + '<span class="ui-li-count">12</span></a></li>';
+           divsList.append(element);
+            
+        });
     
     });
     
