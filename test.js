8c6fce647ce42c9cd34d3800e60d11f19fd316d5
andnazarov <nazaroff.and@gmail.com>
2019-06-11 22:26:29 +0300

    1

--- test.js
+++ test.js
@@ -1,24 +1,27 @@
-$( "*" ).on( "pagecreate", function( event ) {
+$("*").on("pagecreate", function(event) {
+  $.get("divisions.json", function(datajson) {
+    var itemList = datajson; //JSON.parse(datajson);
 
-    $.get("divisions.json", function (datajson) {
-        var itemList = datajson;//JSON.parse(datajson);
-        
-        var divsList = $("#divsList");
-        
-        itemList.forEach(function(item, i, itemList) {
-           
-           var element = '<li id="div' + item.id + '"><a href="#">' + item.id + '. ' + item.caption + '<span class="ui-li-count">' + item.questions_count + '</span></a></li>';
-           divsList.append(element);
-            
-        });
-        
-        divsList.listview("refresh");
-    
+    var divsList = $("#divsList");
+
+    itemList.forEach(function(item, i, itemList) {
+      var element =
+        '<li id="div' +
+        item.id +
+        '"><a href="#">' +
+        item.id +
+        ". " +
+        item.caption +
+        '<span class="ui-li-count">' +
+        item.questions_count +
+        "</span></a></li>";
+      divsList.append(element);
     });
 
+    divsList.listview("refresh");
+  });
 });
 
 $("#divsList > li").on("click", function(ebent) {
-    alert(event);
-
+  alert(event);
 });
