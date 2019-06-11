3df42cb95d8ef8cd971a02888b1504e5ac53ad2d
root <nazaroff.and@gmail.com>
2019-06-11 19:08:33 +0300

    1

--- test.js
+++ test.js
@@ -1,21 +1,3 @@
-/*jQuery(document).ready(function ($) {
-    
-    $.get("divisions.json", function (datajson) {
-        var itemList = datajson;//JSON.parse(datajson);
-        
-        var divsList = $("#divsList");
-        
-        itemList.forEach(function(item, i, itemList) {
-           
-           var element = '<li id="div' + item.id + '"><a href="#">' + item.id + '. ' + item.caption + '<span class="ui-li-count">12</span></a></li>';
-           divsList.append(element);
-            
-        });
-    
-    });
-    
-});*/
-
 $( "*" ).on( "pagecreate", function( event ) {
 
     $.get("divisions.json", function (datajson) {
@@ -25,7 +7,7 @@ $( "*" ).on( "pagecreate", function( event ) {
         
         itemList.forEach(function(item, i, itemList) {
            
-           var element = '<li id="div' + item.id + '"><a href="#">' + item.id + '. ' + item.caption + '<span class="ui-li-count">' + item.questios_count + '</span></a></li>';
+           var element = '<li id="div' + item.id + '"><a href="#">' + item.id + '. ' + item.caption + '<span class="ui-li-count">' + item.questions_count + '</span></a></li>';
            divsList.append(element);
             
         });
