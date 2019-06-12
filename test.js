4c5f0ff4a4400ebaa8bc449b6759f2cf46743399
andnazarov <nazaroff.and@gmail.com>
2019-06-12 23:08:05 +0300

    1

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
 $("*").on("pagecreate", function(event) {
-  $.get("divisions.json", function(datajson) {
-    var itemList = datajson; //JSON.parse(datajson);
+  if (event.target.attributes.id.value === "divisions_page") {
+    divisions_page_create(event);
+  } else if (event.target.attributes.id.value === "questions_page") {
+    questions_page_show(event);
+  }
+});
 
+function divisions_page_create(event) {
+  $.get("divisions.json", function(datajson) {
+    // Помещаем объект для дальнейшего использования.
+    jQuery.data(document.body, "data", datajson);
+
+    var itemList = datajson;
+    console.log(datajson);
     var divsList = $("#divsList");
 
     itemList.forEach(function(item, i, itemList) {
@@ -22,14 +33,45 @@ $("*").on("pagecreate", function(event) {
 
     // Обработка нажатия на раздел.
     $("#divsList li").click(function() {
-      //console.log(this.id);
       var divId = this.id.replace("div", "");
-      //console.log(divId);
       var index = Number.parseInt(divId) - 1;
-      console.log(itemList[index]);
-
-      // Перерисовываем список вопросов текущего раздела.
-
+      jQuery.data(document.body, "current_division_index", index);
     });
   });
-});
+}
+
+// Вызывается перед показом формы списка вопросов.
+function questions_page_show(event) {
+  $("#questions_page").on("pagebeforeshow", function(event) {
+    var current_division_index = jQuery.data(
+      document.body,
+      "current_division_index"
+    );
+
+    // Установка заголовка формы.
+    var current_division_index_string = (current_division_index + 1).toString();
+    var questions_page_header = $("#questions_page_header");
+    questions_page_header.empty();
+    questions_page_header.append(
+      "<h1>Раздел " + current_division_index_string + "</h1>"
+    );
+    questions_page_header.toolbar("refresh");
+
+    var itemList = jQuery.data(document.body, "data");
+    var division = itemList[current_division_index];
+
+    // Перерисовываем список вопросов текущего раздела.
+    var questionsList = $("#questionsList");
+    questionsList.empty();
+    division.questions.forEach(function(item, i, itemList) {
+      var element =
+        '<li id="ques' +
+        item.number +
+        '"><a href="#"><p>' +
+        item.caption +
+        '</p></a></li>';
+      questionsList.append(element);
+    });
+    questionsList.listview("refresh");
+  });
+}
