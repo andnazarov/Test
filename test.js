d8c94617149849947467b0837c49736cb4c402fc
root <nazaroff.and@gmail.com>
2019-06-13 08:20:14 +0300

    1

--- test.js
+++ test.js
@@ -65,7 +65,7 @@ function questions_page_show(event) {
     questionsList.empty();
     division.questions.forEach(function(item, i, itemList) {
       var element =
-        '<li id="ques' +
+        '<li class="wrap" id="ques' +
         item.number +
         '"><a href="#"><p class="ui-li-desc">' +
         item.number +
