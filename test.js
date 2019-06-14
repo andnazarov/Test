332096c9f0a8803d65560c54b2dd82396c9eb0b9
andnazarov <nazaroff.and@gmail.com>
2019-06-14 09:01:57 +0300

    1

--- test.js
+++ test.js
@@ -124,6 +124,8 @@ function question_page_show(event) {
       var questionDescP = $("#question_desc p");
       questionDescP.html(question.text);
       
+      $("#question_text p").html("question.text");
+      
       
       var answersList = $("#answersList");
       answersList.empty();
